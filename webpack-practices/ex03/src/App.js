3940e2501620d8f5988fc71395553615a4b5c51b
bakukun <h1101201@gmail.com>
2025-02-07 15:34:36 +0900

    web-pack finish

--- webpack-practices/ex03/src/App.js
+++ webpack-practices/ex03/src/App.js
@@ -1,6 +1,9 @@
+import './assets/css/style.css';
+
 function App() {
     const App = document.createElement('div');
     App.textContent = "Hello World";
+    App.className = "Header";
     return App;
 }
 
